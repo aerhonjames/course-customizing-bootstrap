9d18baccd4fb5c1b528f30cfffe4f62678d17d9f
AerhonJames <aerhon.james@gmail.com>
2024-03-15 14:38:39 +0800

    Updated: navbar to fix responsive hover and click

--- src/assets/js/components/navbar.js
+++ src/assets/js/components/navbar.js
@@ -1,9 +1,16 @@
 (function(){
     const megaMenuToggles = document.querySelectorAll('.nav-item.dropdown') || [];
+    const cssVarBreakPoint = getComputedStyle(document.documentElement).getPropertyValue('--theme-breakpoint-lg') || '992px';
+    const breakpointLG = parseInt(cssVarBreakPoint, 10);
+
 
     function showMenu(event){
         const dropdown = event.target.querySelector('.dropdown-menu');
         const menuToggle = event.target.querySelector('.dropdown-toggle');
+
+        if(window.innerWidth < breakpointLG){
+            return;
+        }
 
         if(dropdown && menuToggle){
             dropdown.classList.add('show');
@@ -15,6 +22,10 @@
     function hideMenu(event){
         const dropdown = event.target.querySelector('.dropdown-menu');
         const menuToggle = event.target.querySelector('.dropdown-toggle');
+
+        if(window.innerWidth < breakpointLG){
+            return;
+        }
 
         if(dropdown && menuToggle){
             dropdown.classList.remove('show');
